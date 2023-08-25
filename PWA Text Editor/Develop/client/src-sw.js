6c0bf2f8fb2bb0d8b37491adbf8b0db0ada59e2d
nporter <nporterwork@gmail.com>
2023-08-24 23:49:43 -0700

    updated src-sw.js

--- PWA Text Editor/Develop/client/src-sw.js
+++ PWA Text Editor/Develop/client/src-sw.js
@@ -1,12 +1,14 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache files from the manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache pages using CacheFirst strategy
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,7 +16,7 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
     }),
   ],
 });
@@ -26,5 +28,30 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Cache CSS and JavaScript Files using StaleWhileRevalidate strategy
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script',
+  new StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
+);
+
+// Cache Image Files using CacheFirst strategy with expiration
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+// Provide an offline fallback
+offlineFallback({
+  pageFallback: 'offline.html',  // Ensure you have an offline.html page or change to a suitable fallback.
+  imageFallback: 'offline-image.jpg',  // Optional, if you want to provide an offline image.
+});
