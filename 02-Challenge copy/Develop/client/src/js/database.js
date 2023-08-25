b605728f25fcdb69a6f267b1d62534ededb1856f
nporter <nporterwork@gmail.com>
2023-08-24 23:30:31 -0700

    updated database

--- 02-Challenge copy/Develop/client/src/js/database.js
+++ 02-Challenge copy/Develop/client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+let db;
+
+const initdb = async () => {
+  db = await openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -11,11 +13,35 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  if (!db) {
+    console.error('Database has not been initialized.');
+    return;
+  }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add({ content });
+  await tx.done;
+
+  console.log('Content added to the database.');
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  if (!db) {
+    console.error('Database has not been initialized.');
+    return;
+  }
+
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const contents = await store.getAll();
+
+  return contents.map(item => item.content);
+};
 
 initdb();
